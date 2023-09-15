a17a11895e72f50035971eb3e472cef8a02fa517
moviefan322 <philman202@yahoo.com>
2023-09-14 21:50:03 -0400

    adds getters and setters to player class

---
+++ main.py
@@ -0,0 +1,19 @@
+from player import Player
+
+tim = Player("Tim")
+
+print(tim.name)
+print(tim.lives)
+tim.lives -= 1
+print(tim)
+
+tim.lives -= 1
+print(tim)
+
+tim.lives -= 1
+print(tim)
+
+tim.lives -= 1
+print(tim)
+
+
