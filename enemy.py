7537d07a22d2c8d0adff8e5d716945c3a87a8c71
moviefan322 <philman202@yahoo.com>
2023-09-19 18:06:03 -0400

    adds vampire class

--- enemy.py
+++ enemy.py
@@ -4,14 +4,20 @@ class Enemy:
         self.name = name
         self.hit_points = hit_points
         self.lives = lives
+        self.alive = True
 
     def take_damage(self, damage):
         remaining_points = self.hit_points - damage
         if remaining_points >= 0:
             self.hit_points = remaining_points
-            print("I took {} damage and have {} left".format(damage, self.hit_points))
+            print("{} took {} damage and has {} left".format(self.name, damage, self.hit_points))
         else:
-            self.lives -= 1
+            self.lives = self.lives - 1
+            if self.lives > 0:
+                print("{0.name} lost a life".format(self))
+            else:
+                print("{0.name} is dead".format(self))
+                self.alive = False
 
     def __str__(self):
         return "Name: {0.name}; Lives: {0.lives}; HitPoints: {0.hit_points}".format(self)
@@ -25,3 +31,13 @@ class Troll(Enemy):
 
     def grunt(self):
         print("Me {0.name}, {0.name} stomp you".format(self))
+
+
+class Vampire(Enemy):
+
+    def __init__(self, name):
+        # super(Vampire, self).__init__(name=name, lives=1, hit_points=23)
+        super().__init__(name=name, lives=3, hit_points=12)
+
+    def hiss(self):
+        print("Me {0.name}, {0.name} drink your blood".format(self))
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from enemy import Enemy, Troll
+from enemy import Enemy, Troll, Vampire
 
 ghoul = Enemy("Ghoul", 12, 1)
 print(ghoul)
@@ -12,6 +12,18 @@ print("Troll2: {}".format(troll2))
 troll3 = Troll("Urg")
 print(troll, troll2, troll3, sep="\n")
 
+vamp = Vampire("Dracula")
+
 troll.grunt()
 troll2.grunt()
 troll3.grunt()
+
+print(vamp)
+vamp.take_damage(5)
+print(vamp)
+
+troll.take_damage(3)
+
+while vamp.alive:
+    vamp.take_damage(1)
+    print(vamp)
