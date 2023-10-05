ac8ae731b8f2e74e6890f500670ecbe9e51485b1
moviefan322 <philman202@yahoo.com>
2023-10-05 16:26:53 -0400

    adds title to head

--- html_doc.py
+++ html_doc.py
@@ -22,8 +22,11 @@ class DocType(Tag):
 
 class Head(Tag):
 
-    def __init__(self):
+    def __init__(self, title=None):
         super().__init__('head', '')
+        if title:
+            self.title_tag = Tag("title", title)
+            self.contents = str(self.title_tag)
 
 
 class Body(Tag):
@@ -44,9 +47,9 @@ class Body(Tag):
 
 
 class HtmlDoc(object):
-    def __init__(self):
+    def __init__(self, title=None):
         self._doc_type = DocType()
-        self._head = Head()
+        self._head = Head(title)
         self._body = Body()
 
     def add_tag(self, name, contents):
@@ -61,8 +64,9 @@ class HtmlDoc(object):
 
 
 if __name__ == "__main__":
-    my_page = HtmlDoc()
+    my_page = HtmlDoc("Demo HTML doc")
     my_page.add_tag("h1", "Main Heading")
     my_page.add_tag("h2", "sub-heading")
     my_page.add_tag("p", "This is the paragraph that will appear on the page")
-    my_page.display()
+    with open('test.html', 'w') as test_doc:
+        my_page.display(file=test_doc)
