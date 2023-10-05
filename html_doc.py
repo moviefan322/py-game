e9c34aa7de9f9b0fe2e284a9aa8dbc90f67d1d69
moviefan322 <philman202@yahoo.com>
2023-10-05 16:15:47 -0400

    writes html to a file

--- html_doc.py
+++ html_doc.py
@@ -2,21 +2,22 @@ class Tag(object):
 
     def __init__(self, name, contents):
         self.start_tag = '<{}>'.format(name)
-        self.end_tag = '<{}>'.format(name)
+        self.end_tag = '</{}>'.format(name)
         self.contents = contents
 
     def __str__(self):
         return "{0.start_tag}{0.contents}{0.end_tag}".format(self)
 
-    def display(self):
-        print(self)
+    def display(self, file=None):
+        print(self, file=file)
 
 
 class DocType(Tag):
 
     def __init__(self):
-        super().__init__("!DOCTYPE html")
-        self.end_tag = ""  # Doctype doesn't have end tag
+        super().__init__(
+            '!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" http://www.w3.org/TR/html4/strict.dtd', '')
+        self.end_tag = ''   # DOCTYPE doesn't have an end tag
 
 
 class Head(Tag):
@@ -35,8 +36,33 @@ class Body(Tag):
         new_tag = Tag(name, contents)
         self._body_contents.append(new_tag)
 
-    def display(self):
+    def display(self, file=None):
         for tag in self._body_contents:
             self.contents += str(tag)
 
-        super().display()
+        super().display(file=file)
+
+
+class HtmlDoc(object):
+    def __init__(self):
+        self._doc_type = DocType()
+        self._head = Head()
+        self._body = Body()
+
+    def add_tag(self, name, contents):
+        self._body.add_tag(name, contents)
+
+    def display(self, file=None):
+        self._doc_type.display(file=file)
+        print("<html>")
+        self._head.display(file=file)
+        self._body.display(file=file)
+        print("</html>")
+
+
+if __name__ == "__main__":
+    my_page = HtmlDoc()
+    my_page.add_tag("h1", "Main Heading")
+    my_page.add_tag("h2", "sub-heading")
+    my_page.add_tag("p", "This is the paragraph that will appear on the page")
+    my_page.display()
